fa5e2b91af42bb7fb9d30d7ad3a0b165db90ad05
feifan-agillapro <feifansnf@gmail.com>
2015-10-02 13:57:59 -0400

    mysql config file restructured

---
+++ app/core/mysql_config.php
@@ -0,0 +1,12 @@
+<?php
+/*
+$hostname = "mysql6.000webhost.com";
+$db_name = "a7603014_champ";
+$username = "a7603014_root";
+$password = "Cde3xsw@";
+*/
+$hostname = "localhost";
+$db_name = "championcounter";
+$username = "root";
+$password = "mysql";
+?>
