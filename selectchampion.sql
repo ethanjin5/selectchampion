37bb465c8c515eddcd18b97b947b9e668cdf3672
feifan-agillapro <feifansnf@gmail.com>
2015-10-13 16:41:18 -0400

    sql file updated

--- selectchampion.sql
+++ selectchampion.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 12, 2015 at 04:19 PM
+-- Generation Time: Oct 13, 2015 at 09:40 PM
 -- Server version: 5.6.23
 -- PHP Version: 5.3.29
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `selectchampion`
 --
-CREATE DATABASE IF NOT EXISTS `selectchampion` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `selectchampion`;
 
 -- --------------------------------------------------------
 
@@ -41,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `champion_list` (
   `skill_e` text,
   `skill_r` text,
   `tags` varchar(1000) DEFAULT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=129 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `champion_list`
@@ -87,7 +85,94 @@ INSERT INTO `champion_list` (`id`, `name`, `eng_name`, `alias`, `keywords`, `des
 (37, '艾瑞莉娅', 'Irelia', '刀锋意志', '女刀', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (38, '迦娜', 'Janna', '风暴之怒', '风女,凤女', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (39, '嘉文四世', 'JarvanIV', '德玛西亚皇子', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(40, '贾克斯', 'Jax', '武器大师', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(40, '贾克斯', 'Jax', '武器大师', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(41, '杰斯', 'Jayce', '未来守护者', NULL, '武装着睿智与魅力，以及标志性的可变形铁锤，杰斯毕生致力于守护他的祖国，皮尔特沃夫。靠着发明家的身份而闻达于世的他，用充满磁性的人格以及创意新颖的作品照亮了城邦人民的生活。不过，他的诸多技术奇迹也吸引了皮尔特沃夫的敌人们的注意力。维克托，这个用机械强化身体的祖安科学家，对杰斯的实验室发动了一次致命的袭击。在受到实验室贫乏的安防部队那形同虚设的阻碍后，维克托偷到了一个奥能转换器的设计图。杰斯担心维克托会用偷来的技术制造毁灭性武器。他恳求皮尔特沃夫政府作出回击，但官员们不愿意对一个侵略性的行为作出支持。他决定亲自处理，因为他意识到，如果现在还不还击的话，皮尔特沃夫将永远不得安宁。\r\n\r\n杰斯回到了他的实验室，并开始着手准备他的孤身作战计划。在经过了一段研究、开发、手动测试的紧张时期后，他露面了。他制造了一系列的强力军备，包括他的至高成就——多功能、可变形的墨丘利之锤。在全副武装、准备妥当后，杰斯发起了进攻，轻易地在维克托的助手们冲过来阻止他的时候将他们荡平。杰斯一路猛进至实验室的心脏地带，并与维克托交手，迫使这个祖安人逃之夭夭。在摧毁了维克托那还在原型阶段的武器后，杰斯回到了皮尔特沃夫，并在那儿受到了英雄般的待遇。他沉醉在赞美声中，但他知道，他的行为已经触怒了城邦的敌人们。现在，全身心保护皮城人民的杰斯，是皮尔特沃夫通向光明未来的希望。\r\n\r\n“相信我：如果我们够机灵的话，皮尔特沃夫就能强势地面对任何威胁。我就是个活生生的例子。”——杰斯', NULL, NULL, NULL, NULL, NULL, NULL),
+(42, '金克丝', 'Jinx', '暴走萝莉', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(43, '卡莉丝塔', 'Kalista', '复仇之矛', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(44, '卡尔玛', 'Karma', '天启者', '扇子妈', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(45, '卡尔萨斯', 'Karthus', '死亡颂唱者', '死歌', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(46, '卡萨丁', 'Kassadin', '虚空行者', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(47, '卡特琳娜', 'Katarina', '不祥之刃', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(48, '凯尔', 'Kayle', '审判天使', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(50, '凯南', 'Kennen', '狂暴之心', '电耗子', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(51, '卡兹克', 'Khazix', '虚空掠夺者', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(52, '克格莫', 'KogMaw', '深渊巨口', '大嘴', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(53, '乐芙兰', 'Leblanc', '诡术妖姬', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(54, '李青', 'LeeSin', '盲僧', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(55, '蕾欧娜', 'Leona', '曙光女神', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(56, '丽桑卓', 'Lissandra', '冰霜女巫', '冰女', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(57, '卢锡安', 'Lucian', '圣枪游侠', '奥巴马', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(58, '璐璐', 'Lulu', '仙灵女巫', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(59, '拉克丝', 'Lux', '光辉女郎', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(60, '墨菲特', 'Malphite', '熔岩巨兽', '石头人', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(61, '玛尔扎哈', 'Malzahar', '虚空先知', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(62, '茂凯', 'Maokai', '扭曲树精', '树人,大树', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(63, '易', 'MasterYi', '无极剑圣', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(64, '赏金猎人', 'MissFortune', '厄运小姐', '女枪', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(65, '孙悟空', 'MonkeyKing', '齐天大圣', '猴哥,猴子', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(66, '莫德凯撒', 'Mordekaiser', '金属大师', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(67, '莫甘娜', 'Morgana', '堕落天使', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(68, '唤潮鲛姬', 'Nami', '娜美', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(69, '内瑟斯', 'Nasus', '沙漠死神', '狗头', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(70, '诺提勒斯', 'Nautilus', '深海泰坦', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(71, '奈德丽', 'Nidalee', '狂野女猎手', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(72, '永恒梦魇', 'Nocturne', '魔腾', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(49, 'new', 'Kindred', 'd', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(74, '努努', 'Nunu', '雪人骑士', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(75, '奥拉夫', 'Olaf', '狂战士', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(76, '奥莉安娜', 'Orianna', '发条魔灵', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(77, '潘森', 'Pantheon', '战争之王', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(78, '波比', 'Poppy', '钢铁大使', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(79, '奎因', 'Quinn', '德玛西亚之翼', '鸟人', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(80, '拉莫斯', 'Rammus', '披甲龙龟', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(81, '雷克塞', 'RekSai', '虚空遁地兽', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(82, '雷克顿', 'Renekton', '荒漠屠夫', '鳄鱼', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(83, '雷恩加尔', 'Rengar', '傲之追猎者', '狮子狗', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(84, '锐雯', 'Riven', '放逐之刃', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(85, '兰博', 'Rumble', '机械公敌', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(86, '瑞兹', 'Ryze', '流浪法师', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(87, '瑟庄妮', 'Sejuani', '凛冬之怒', '猪女', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(88, '萨科', 'Shaco', '恶魔小丑', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(89, '慎', 'Shen', '暮光之眼', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(90, '希瓦娜', 'Shyvana', '龙血武姬', '龙女', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(91, '辛吉德', 'Singed', '炼金术士', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(92, '赛恩', 'Sion', '亡灵战神', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(93, '希维尔', 'Sivir', '战争女神', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(94, '斯卡纳', 'Skarner', '水晶先锋', '蝎子', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(95, '娑娜', 'Sona', '琴瑟仙女', '琴女', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(96, '索拉卡', 'Soraka', '众星之子', '奶妈', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(97, '斯维因', 'Swain', '策士统领', '乌鸦', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(98, '辛德拉', 'Syndra', '暗黑元首', '球女', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(99, '塔姆', 'TahmKench', '河流之王', '大蛤蟆', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(100, '泰隆', 'Talon', '刀锋之影', '男刀', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(101, '塔里克', 'Taric', '宝石骑士', '宝石', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(102, '提莫', 'Teemo', '迅捷斥候', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(103, '锤石', 'Thresh', '魂锁典狱长', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(104, '崔丝塔娜', 'Tristana', '麦林炮手', '小炮,炮娘', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(105, '巨魔之王', 'Trundle', '特朗德尔', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(106, '泰达米尔', 'Tryndamere', '蛮族之王', '蛮王', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(107, '崔斯特', 'TwistedFate', '卡牌大师', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(108, '图奇', 'Twitch', '瘟疫之源', '老鼠', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(109, '乌迪尔', 'Udyr', '兽灵行者', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(110, '厄加特', 'Urgot', '首领之傲', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(111, '韦鲁斯', 'Varus', '惩戒之箭', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(112, '薇恩', 'Vayne', '暗夜猎手', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(113, '维迦', 'Veigar', '邪恶小法师', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(114, '维克兹', 'Velkoz', '虚空之眼', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(115, '蔚', 'Vi', '皮城执法官', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(116, '维克托', 'Viktor', '机械先驱', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(117, '弗拉基米尔', 'Vladimir', '猩红收割者', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(118, '沃利贝尔', 'Volibear', '雷霆咆哮', '狗熊', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(119, '沃里克', 'Warwick', '嗜血猎手', '狼人', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(120, '泽拉斯', 'Xerath', '远古巫灵', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(121, '赵信', 'XinZhao', '德邦总管', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(122, '亚索', 'Yasuo', '疾风剑豪', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(123, '约里克', 'Yorick', '掘墓者', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(124, '扎克', 'Zac', '生化魔人', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(125, '劫', 'Zed', '影流之主', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(126, '吉格斯', 'Ziggs', '爆破鬼才', '炸弹人', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(127, '基兰', 'Zilean', '时光守护者', '时光老头儿', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(128, '婕拉', 'Zyra', '荆棘之兴', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -110,9 +195,9 @@ CREATE TABLE IF NOT EXISTS `counter` (
 --
 
 INSERT INTO `counter` (`id`, `type`, `category`, `champion_weak`, `champion_strong`, `upvote`, `downvote`) VALUES
-(1, '上单', 'strong', 1, 40, 166, 39),
-(2, '上单', 'strong', 1, 25, 101, 21),
-(3, '上单', 'strong', 18, 1, 59, 28);
+(1, '上单', 'strong', 1, 40, 167, 38),
+(2, '上单', 'strong', 1, 25, 101, 20),
+(3, '上单', 'strong', 18, 1, 61, 28);
 
 -- --------------------------------------------------------
 
@@ -124,17 +209,41 @@ CREATE TABLE IF NOT EXISTS `counter_tips` (
   `id` int(10) NOT NULL,
   `counter_id` int(10) NOT NULL,
   `tip` text NOT NULL,
-  `date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `author` varchar(30) NOT NULL,
+  `date_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `author` varchar(30) NOT NULL DEFAULT '匿名',
   `vote` int(11) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `counter_tips`
 --
 
 INSERT INTO `counter_tips` (`id`, `counter_id`, `tip`, `date_time`, `author`, `vote`) VALUES
-(1, 1, 'Jax does really well against aatrox in lane, his e countrs aatroxes w, his damage out scales aatroxes, jax also wins trades in lane better, as a jax this lane isnt easy to lose', '2015-10-08 08:53:00', 'Feifan', 0);
+(1, 1, 'Jax does really well against aatrox in lane, his e countrs aatroxes w, his damage out scales aatroxes, jax also wins trades in lane better, as a jax this lane isnt easy to lose', '2015-10-12 19:07:41', 'Feifan', 3),
+(2, 1, 'this is crazy matchup,this is crazy matchup,this is crazy matchup.', '2015-10-12 19:07:41', 'Feifan', 1),
+(3, 1, 'qwerwdsfds', '2015-10-13 17:39:44', 'asdf', 1),
+(4, 1, 'sdfddddddd', '2015-10-13 17:42:06', 'asdf', 0),
+(5, 1, 's', '2015-10-13 17:42:26', 's', 0),
+(6, 1, 'fdd', '2015-10-13 17:46:26', 'sd', 1),
+(7, 1, 'sdd', '2015-10-13 17:46:59', 'sd', 0),
+(8, 1, 'iii', '2015-10-13 17:50:13', 'kk', 0),
+(9, 1, 'ssss', '2015-10-13 17:53:54', 'ddd', 0),
+(10, 1, 'testing', '2015-10-13 18:03:01', 'feifan', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tier`
+--
+
+CREATE TABLE IF NOT EXISTS `tier` (
+  `god` varchar(1000) DEFAULT NULL,
+  `1` varchar(1000) DEFAULT NULL,
+  `2` varchar(1000) DEFAULT NULL,
+  `3` varchar(1000) DEFAULT NULL,
+  `4` varchar(1000) DEFAULT NULL,
+  `explained` varchar(1000) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
@@ -166,7 +275,7 @@ ALTER TABLE `counter_tips`
 -- AUTO_INCREMENT for table `champion_list`
 --
 ALTER TABLE `champion_list`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=41;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=129;
 --
 -- AUTO_INCREMENT for table `counter`
 --
@@ -176,7 +285,7 @@ ALTER TABLE `counter`
 -- AUTO_INCREMENT for table `counter_tips`
 --
 ALTER TABLE `counter_tips`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
